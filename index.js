56381c26cbe68f8149e19f7832c2a1964f1c2526
man7788 <jerman1841@gmail.com>
2023-09-10 04:35:38 +0800

    Migrate app to Express

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-// Server
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-http
-  .createServer((req, res) => {
-    const q = new URL(req.url, "https://localhost:8080/");
-    let filename;
+app.get("/", (req, res) => {
+  res.sendFile("/index.html", { root: __dirname + "/public" });
+});
 
-    if (q.pathname === "/") {
-      filename = "./index.html";
-    } else {
-      filename = "./" + q.pathname + ".html";
-    }
+app.get("/about", (req, res) => {
+  res.sendFile("/about.html", { root: __dirname + "/public" });
+});
 
-    fs.readFile(filename, function (err, data) {
-      if (err) {
-        fs.readFile("./404.html", function (err, data) {
-          res.writeHead(404, { "Content-Type": "text/html" });
-          res.end(data);
-        });
-      } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      }
-    });
-  })
-  .listen(8080);
+app.get("/contact-me", (req, res) => {
+  res.sendFile("/contact-me.html", { root: __dirname + "/public" });
+});
+
+app.use(function (req, res) {
+  res.status(404).sendFile("/404.html", { root: __dirname + "/public" });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
